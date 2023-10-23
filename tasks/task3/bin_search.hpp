dd5af51a5f9c77240143e7185612a5c6a9f57dd8
katescha8 <kate.schapo.5@gmail.com>
2023-10-23 18:40:17 +0300

    task 3 tests passed

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
 int* LowerBound(int* first, int* last, int value) {
-	throw std::runtime_error("not implemented error");
+	int* it = first;
+	int count = std::distance(first, last);
+	int step = count;
+
+	while (count > 0)
+    {
+        it = first; 
+        step = count / 2; 
+        std::advance(it, step);
+ 
+        if (*it < value)
+        {
+            first = ++it; 
+            count -= step + 1; 
+        }
+        else
+            count = step;
+    }
+ 
+    return first;
 }
